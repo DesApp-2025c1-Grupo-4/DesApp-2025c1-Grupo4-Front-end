bf68b5309481f932e17e29581a881f3a87ffaf9e
Paula Ricci <ricci.paula.e@gmail.com>
2025-05-18 05:24:16 -0300

    agrego carpeta assets

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from '@mui/material';
 import { customMuiTheme } from './config/customMuiTheme';
 
 import { App } from './App';
-import './index.css'
+import './assets/css/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={customMuiTheme}>
